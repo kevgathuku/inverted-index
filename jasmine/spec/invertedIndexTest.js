c4a022c67c107bfebf22feed20ffc5399906270b
Kevin Ndung'u <kevgathuku@gmail.com>
2015-10-26 09:50:20 +0300

    Add create index test

--- jasmine/spec/invertedIndexTest.js
+++ jasmine/spec/invertedIndexTest.js
@@ -4,20 +4,27 @@ describe('Inverted Index Tests: ', function() {
   var results;
 
   beforeEach(function(done) {
-    index.createIndex('books.json').done(function(data) {
+    index.createIndex('books.json', index.populateIndex).done(function(data) {
       results = data;
 
       // Invoke jasmine's done callback
       done();
-    })
+    });
   });
 
   describe('Reads book data', function() {
 
-    it("reads the JSON file successfully", function() {
+    it('reads the JSON file successfully', function() {
       expect(results).not.toBeUndefined();
       expect(results.length).not.toEqual(0);
       expect(results.length).toEqual(2);
+    })
+  });
+
+  describe('Populates Index', function() {
+
+    it('creates the index once JSON file has been read', function() {
+      expect(index.results).not.toEqual({});
     })
   });
 });
