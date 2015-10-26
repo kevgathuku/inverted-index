f379688c54ca29ba1c613fb3013263f7100a4ae0
Kevin Ndung'u <kevgathuku@gmail.com>
2015-10-26 17:44:27 +0300

    Add tests to verifies correct index mapping
    Objects in the first Object in the array should return 0
    Objects in the 2nd Object in the array should return 1

--- jasmine/spec/invertedIndexTest.js
+++ jasmine/spec/invertedIndexTest.js
@@ -24,6 +24,9 @@ describe('Inverted Index Tests: ', function() {
 
   describe('Populates Index', function() {
 
+    var index0 = ['alice', 'rabbit', 'imagination', 'world'];
+    var index1 = ['alliance', 'elf', 'dwarf', 'wizard'];
+
     it('creates the index once JSON file has been read', function() {
       expect(index.results).not.toEqual({});
     });
@@ -47,6 +50,32 @@ describe('Inverted Index Tests: ', function() {
       var keys = Object.keys(index.results);
       for (var i = 0, len = keys.length; i < len; i++) {
         expect(filter.stopWords.indexOf(keys[i])).toBe(-1);
+      }
+    });
+
+    it('creates index containing correct terms', function() {
+      var keys = Object.keys(index.results);
+
+      for (var i = 0, len = index0.length; i < len; i++) {
+        expect(keys).toContain(index0[i]);
+      }
+
+      for (var i = 0, len = index1.length; i < len; i++) {
+        expect(keys).toContain(index0[i]);
+      }
+    });
+
+    it('maps string keys to the correct objects', function() {
+      var keys = Object.keys(index.results);
+
+      // Should return 0 for objects in the first array element
+      for (var i = 0, len = index0.length; i < len; i++) {
+        expect(index.results[index0[i]]).toBe(0);
+      }
+
+      // Should return 1 for objects in the second array element
+      for (var i = 0, len = index0.length; i < len; i++) {
+        expect(index.results[index1[i]]).toBe(1);
       }
     });
   });
